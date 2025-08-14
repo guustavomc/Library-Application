af05141cdf70ddc5eeb7fba7a1a7e3fcac12856c
guustavomc <gustavomccb@gmail.com>
2025-08-13 22:15:29 -0300

    added post books and get books by id

--- main.go
+++ main.go
@@ -1,26 +1,63 @@
 package main
 
 import (
+	"fmt"
 	"library/models"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
+
+var books = []models.Book{
+	{ID: 1, Name: "LOTR 1", Price: 50.0},
+	{ID: 2, Name: "GOT", Price: 33.1},
+	{ID: 3, Name: "Star wars", Price: 70.0},
+}
 
 func main() {
 	router := gin.Default()
 
 	router.GET("/books", getBooks)
+	router.POST("/books", postBooks)
+	router.GET("/books/:id", getBooksById)
+
 	router.Run()
 }
 
 func getBooks(c *gin.Context) {
-	var books = []models.Book{
-		{ID: 1, Name: "LOTR 1", Price: 50.0},
-		{ID: 2, Name: "GOT", Price: 33.1},
-		{ID: 3, Name: "Star wars", Price: 70.0},
-	}
+
 	c.JSON(200, gin.H{
 		"book": books,
 	})
 
+}
+
+func getBooksById(c *gin.Context) {
+	idParam := c.Param("id")
+	fmt.Println(idParam)
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"erro": err.Error()})
+		return
+	}
+	for _, p := range books {
+		if p.ID == id {
+			c.JSON(200, p)
+			return
+		}
+	}
+
+	c.JSON(404, gin.H{"message": "Book Not Found"})
+}
+
+func postBooks(c *gin.Context) {
+	var newBook models.Book
+	if err := c.ShouldBindJSON(&newBook); err != nil {
+		c.JSON(400, gin.H{
+			"erro": err.Error()})
+		return
+	}
+
+	books = append(books, newBook)
 }
